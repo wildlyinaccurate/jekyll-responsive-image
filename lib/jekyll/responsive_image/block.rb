281ec01fb46d7326bf50251d95dc525108fcc272
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-12-07 22:03:26 +0000

    include Utils rather than calling static methods

--- lib/jekyll/responsive_image/block.rb
+++ lib/jekyll/responsive_image/block.rb
@@ -1,6 +1,8 @@
 module Jekyll
   class ResponsiveImage
     class Block < Liquid::Block
+      include ResponsiveImage::Utils
+
       def render(context)
         config = ResponsiveImage.defaults.dup
         config.merge!(context.registers[:site].config['responsive_image'])
@@ -10,7 +12,7 @@ module Jekyll
 
         resize_handler = ResizeHandler.new
         img = Magick::Image::read(attributes['path']).first
-        attributes['original'] = Utils.image_hash(attributes['path'], img.columns, img.rows)
+        attributes['original'] = image_hash(attributes['path'], img.columns, img.rows)
         attributes['resized'] = resize_handler.resize_image(img, config)
 
         partial = File.read(image_template)
