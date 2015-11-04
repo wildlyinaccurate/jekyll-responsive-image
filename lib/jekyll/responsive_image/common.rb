39085959fcf5668680c7a1f22eed4f69ed98b7af
Joseph Wynn <joseph.wynn@bbc.co.uk>
2015-11-04 22:55:32 +0000

    Copy generated files to _site and add output directory to keep_files

---
+++ lib/jekyll/responsive_image/common.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class ResponsiveImage
+    module Common
+      include Jekyll::ResponsiveImage::Utils
+
+      def make_config(site)
+        config = ResponsiveImage.defaults.dup.merge(site.config['responsive_image']).merge(:site_dest => site.dest)
+
+        # Not very nice, but this is needed to create a clean path to add to keep_files
+        output_dir = format_output_path(config['output_path_format'], '*', '*', '*')
+        site.config['keep_files'] << output_dir unless site.config['keep_files'].include?(output_dir)
+
+        config
+      end
+    end
+  end
+end
