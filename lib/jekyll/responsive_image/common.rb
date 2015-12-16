503d64a5ff91978830e0da2cff4f3150d3ee479e
Boris SCHAPIRA <borisschapira@gmail.com>
2015-12-16 09:04:57 +0100

    Managing base path to file in resized versions

--- lib/jekyll/responsive_image/common.rb
+++ lib/jekyll/responsive_image/common.rb
@@ -7,7 +7,7 @@ module Jekyll
         config = ResponsiveImage.defaults.dup.merge(site.config['responsive_image']).merge(:site_dest => site.dest)
 
         # Not very nice, but this is needed to create a clean path to add to keep_files
-        output_dir = format_output_path(config['output_path_format'], '*', '*', '*')
+        output_dir = format_output_path(config['output_path_format'], config['base_path'], '*', '*', '*')
         site.config['keep_files'] << output_dir unless site.config['keep_files'].include?(output_dir)
 
         config
--- lib/jekyll/responsive_image/defaults.rb
+++ lib/jekyll/responsive_image/defaults.rb
@@ -1,6 +1,7 @@
 module Jekyll
   class ResponsiveImage
     @defaults = {
+      'base_path' => 'assets/images',
       'default_quality'    => 85,
       'output_path_format' => 'assets/resized/%{filename}-%{width}x%{height}.%{extension}',
       'sizes'              => [],
