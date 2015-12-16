434dad199f7c98247d993ea95ee3f476532d069d
Boris SCHAPIRA <borisschapira@gmail.com>
2015-12-16 09:16:59 +0100

    Coherence concerning documentation and default

--- lib/jekyll/responsive_image/defaults.rb
+++ lib/jekyll/responsive_image/defaults.rb
@@ -1,7 +1,7 @@
 module Jekyll
   class ResponsiveImage
     @defaults = {
-      'base_path' => 'assets/images',
+      'base_path' => 'assets',
       'default_quality'    => 85,
       'output_path_format' => 'assets/resized/%{filename}-%{width}x%{height}.%{extension}',
       'sizes'              => [],
