76f834ce09e89f2a7b9df7a2390d33e65380e3ba
Joseph Wynn <joseph.wynn@bbc.co.uk>
2015-12-26 18:11:05 +0000

    Add `dirname` to image hashes. Closes #5.

--- lib/jekyll/responsive_image/utils.rb
+++ lib/jekyll/responsive_image/utils.rb
@@ -18,6 +18,7 @@ module Jekyll
       def image_hash(path, width, height)
         {
           'path'      => path,
+          'dirname'   => File.dirname(path),
           'basename'  => File.basename(path),
           'filename'  => File.basename(path, '.*'),
           'extension' => File.extname(path).delete('.'),
