7d2a3dd514f1cdf0f8c9b3252e9a1940c3365bd0
Joseph Wynn <joseph@wildlyinaccurate.com>
2016-09-25 11:55:59 +0100

    Move 'fixtures' to 'test-site' because, like, that's what it is

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,8 +2,8 @@ Before do
   FileUtils.rm_rf(TEST_DIR) if File.exist?(TEST_DIR)
   FileUtils.mkdir_p(TEST_DIR)
 
-  fixtures = File.expand_path('../../fixtures', __FILE__)
-  FileUtils.cp_r(Dir.glob("#{fixtures}/*"), TEST_DIR)
+  test_site = File.expand_path('../../test-site', __FILE__)
+  FileUtils.cp_r(Dir.glob("#{test_site}/*"), TEST_DIR)
 
   Dir.chdir(TEST_DIR)
 end
