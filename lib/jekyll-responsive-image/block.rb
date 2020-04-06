f49726affc47148d881b5eada69dadd24f74832b
Joseph Wynn <joseph@wildlyinaccurate.com>
2020-04-06 21:57:43 +1200

    Automatically convert tabbed indentation to spaces

--- lib/jekyll-responsive-image/block.rb
+++ lib/jekyll-responsive-image/block.rb
@@ -4,7 +4,13 @@ module Jekyll
       include Jekyll::ResponsiveImage::Utils
 
       def render(context)
-        attributes = YAML.load(super)
+        content = super
+
+        if content.include?("\t")
+          content = content.lines.map {|line| line.gsub(/\G[\t ]/, "  ")}.join("\n")
+        end
+
+        attributes = YAML.load(content)
         Renderer.new(context.registers[:site], attributes).render_responsive_image
       end
     end
